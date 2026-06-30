5baaef5abe8791822c5badcc8caab33b42fe0ffe
Sunil <sunilpandab37@gmail.com>
2026-06-30 11:02:15 +0530

    fix of axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,7 @@ const API_BASE_URL = process.env.REACT_APP_API_URL
           const config = {
             headers: { 'Authorization': `Bearer ${token}` }
           };
-          const response = await axios.get('${API_BASE_URL}/users/me', config);
+          const response = await axios.get(`${API_BASE_URL}/users/me`, config);
           const userData = response.data;
           setUser(userData);
           
